0e04a63d12ca00216c4df797d43823fc23f184f5
Sano <36809574+Sanokei@users.noreply.github.com>
2021-11-24 02:26:44 -0500

    got rid of the box colliders in favor of just distance

--- Assets/Script/ActivateTask.cs
+++ Assets/Script/ActivateTask.cs
@@ -1,25 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ActivateTask : MonoBehaviour
 {
-    [SerializeField]
+    private NavMeshAgent _navMeshAgent;
     private GameObject _exclamation;
-    private bool _inBounds = false;
+    private Camera _camera;
 
+    void Start()
+    {
+        _exclamation =  Resources.Load<GameObject>("Prefab/Exclamation Point");
+        _camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        _navMeshAgent = GameObject.Find("Player").GetComponent<NavMeshAgent>();
+    }
     void Update()
     {
-        if(_inBounds && transform.childCount == 0){
-            Instantiate(_exclamation, transform.position + new Vector3(0, 1, 0), Quaternion.identity, transform);
+        if(Vector3.Distance(gameObject.transform.position,_navMeshAgent.transform.position) < 2f && transform.childCount == 0){
+             StartActivateTask();
         }
     }
-    void OnTriggerEnter(Collider other)
+
+    void StartActivateTask()
     {
-        _inBounds = true;
+        GameObject task = Instantiate(_exclamation, transform.position + new Vector3(0, 1, 0), Quaternion.identity, transform);
+        task.GetComponent<TaskActivated>()._camera = _camera;
     }
-    void OnTriggerExit(Collider other)
-    {
-        _inBounds = false;
-    }
+
 }
--- Assets/Script/PlayerNavMesh.cs
+++ Assets/Script/PlayerNavMesh.cs
@@ -12,16 +12,21 @@ public class PlayerNavMesh : MonoBehaviour
 
     [SerializeField]
     private GameObject[] _tasks;
+    [SerializeField]
+    private Transform[] _locationToRest;
 
     //go to a random task location
     public void GoToRandomTask()
     {
-        int randomTaskIndex;
-        do{
-            randomTaskIndex = Random.Range(0, _tasks.Length);
+        GameObject[] filteredTask = System.Array.FindAll(_tasks, x => x.transform.childCount == 0);
+        if(filteredTask.Length > 0)
+        {
+            _navMeshAgent.SetDestination(filteredTask[Random.Range(0,filteredTask.Length)].transform.position);
         }
-        while(_tasks[randomTaskIndex].transform.childCount != 0);
-        _navMeshAgent.SetDestination(_tasks[randomTaskIndex].transform.position);
+        else
+        {
+            _navMeshAgent.SetDestination(_locationToRest[Random.Range(0,_locationToRest.Length)].position);
+        }
     }
 
     //call GoToRandomTask() when the navMeshAgent has no other destination
